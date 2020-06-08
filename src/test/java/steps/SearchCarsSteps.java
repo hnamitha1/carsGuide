61b71380a99d16f9ea0fe30c24a301414e0d4f79
hnamitha1 <namitha.h@gmail.com>
2020-06-08 21:26:03 +0100

    completed step def file

--- src/test/java/steps/SearchCarsSteps.java
+++ src/test/java/steps/SearchCarsSteps.java
@@ -1,63 +1,62 @@
 package steps;
 
+import java.util.List;
+
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import pages.actions.CarsGuideHomePageActions;
+import pages.actions.CarsSearchPageActions;
+import utils.SeleniumDriver;
 
 public class SearchCarsSteps {
 	
+	CarsGuideHomePageActions carsGuideHomePageActions = new CarsGuideHomePageActions();
+	CarsSearchPageActions carsSearchPageActions =  new CarsSearchPageActions();
+	
 	@Given("^I am on the Home Page \"([^\"]*)\" of CarsGuide$")
-	public void i_am_on_the_Home_Page_of_CarsGuide(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void i_am_on_the_Home_Page_of_CarsGuide(String websiteURL) throws Throwable {
+	   SeleniumDriver.openPage(websiteURL);
 	}
 
 	@When("^I move to the menu$")
-	public void i_move_to_the_menu(DataTable arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    // For automatic transformation, change DataTable to one of
-	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
-	    throw new PendingException();
+	public void i_move_to_the_menu(List<String> list) throws Throwable {
+	    String menu = list.get(1);
+	    System.out.println(menu);
+	    carsGuideHomePageActions.moveToBuyAndSellMenu();
 	}
 
 	@And("^click on \"([^\"]*)\" link$")
 	public void click_on_link(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    carsGuideHomePageActions.clickOnSearchCarsMenu();
 	}
 
 	@And("^select carbrand as \"([^\"]*)\" from AnyMake dropDown$")
-	public void select_carbrand_as_from_AnyMake_dropDown(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void select_carbrand_as_from_AnyMake_dropDown(String carBrand) throws Throwable {
+	   carsSearchPageActions.selectCarMake(carBrand);
 	}
 
 	@And("^select carmodel as \"([^\"]*)\" from SelectModel dropDown$")
-	public void select_carmodel_as_from_SelectModel_dropDown(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void select_carmodel_as_from_SelectModel_dropDown(String carModel) throws Throwable {
+	    carsSearchPageActions.selectCarModel(carModel);
 	}
 
 	@And("^select location as \"([^\"]*)\" from SelectLocation dropDown$")
-	public void select_location_as_from_SelectLocation_dropDown(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void select_location_as_from_SelectLocation_dropDown(String location) throws Throwable {
+	    carsSearchPageActions.selectLocation(location);
 	}
 
 	@And("^select price as \"([^\"]*)\" from price dropDown$")
-	public void select_price_as_from_price_dropDown(String arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void select_price_as_from_price_dropDown(String carPrice) throws Throwable {
+	    carsSearchPageActions.selectCarPrice(carPrice);
 	}
 
 	@And("^click on Find My Next Car button$")
 	public void click_on_Find_My_Next_Car_button() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    carsSearchPageActions.clickOnFindMyNextCarButton();
 	}
 
 	@Then("^I should see list of searched cars$")
